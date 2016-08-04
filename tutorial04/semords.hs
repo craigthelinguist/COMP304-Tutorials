e73a8889b0ace1774d282908e85e0cfe4c8d753f
craigthelinguist <aaroncraig@protonmail.ch>
2016-08-04 21:45:04 +1200

    Simplify semord code

--- tutorial04/semords.hs
+++ tutorial04/semords.hs
@@ -3,8 +3,7 @@
 -- A semordnilap is a pair of words (a,b) where a is b spelled backwards.
 
 longestSemord strs =
-   let allPairs = [(x,y) | x <- strs, y <- strs, x /= y]
-       semords  = filter (\(x,y) -> reverse x == y) allPairs
+   let semords = [(x,y) | x <- strs, y <- strs, x /= y, reverse x == y]
        longer (a,b) (c,d) = if length a > length c then (a,b) else (c,d)
     in foldl1 longer semords
 
